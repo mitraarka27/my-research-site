890bcec29d71599fa844a0909402010b639fbd3e
Arka Mitra <mitraarka27@gmail.com>
2025-10-07 22:41:34 -0700

    Fix Vite alias for @/ and case-sensitive imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,20 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx(),
-  ],
+  integrations: [tailwind(), mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
   },
 });
 
